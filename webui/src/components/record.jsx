cc2b067a8f0866c11d67697beb049dc715f5a525
Jussi Steenari <jsteenar@csc.fi>
2021-04-30 14:41:23 +0300

    Removed hardcoded doi
    Signed-off-by: Jussi Steenari <jsteenar@csc.fi>

--- webui/src/components/record.jsx
+++ webui/src/components/record.jsx
@@ -610,7 +610,7 @@ const Record = React.createClass({
             serverCache.createRecordVersion(record, newRecordID => browser.gotoEditRecord(newRecordID));
         }
         const state = record.get('metadata').get('publication_state');
-        const doi = "http://doi.org/10.23728/fmi-b2share.84832a794ea5442896acd5567944fb3b"//record.get('metadata').get('DOI')
+        const doi = record.get('metadata').get('DOI')
         const showB2Note = serverCache.getInfo().get('show_b2note');
         return (
             <div className="container-fluid">

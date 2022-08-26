b8694e37bf1e3219f1b12b325e6105d7cdb3d99c
Harri Hirvonsalo <harri.hirvonsalo@csc.fi>
2022-08-26 14:20:25 +0300

    Fix dev.fmi file download issue
    * BROKER_URL was overwriting values obtained from Vault
    
    Co-Authored-By: Johannes Lares <johannes.lares@csc.fi>

--- b2share/config.py
+++ b2share/config.py
@@ -314,7 +314,7 @@ CACHE_TYPE='redis'
 # Celery
 # ======
 #: Default broker (RabbitMQ on locahost).
-BROKER_URL = "amqp://guest:guest@localhost:5672//"
+# BROKER_URL = "amqp://guest:guest@localhost:5672//"
 #: Default Celery result backend.
 CELERY_RESULT_BACKEND = "redis://localhost:6379/1"
 #: Accepted content types for Celery.

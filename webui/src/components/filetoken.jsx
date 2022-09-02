187143520952aba6c4433825da0ed8f0196cfc17
Joni Tahvanainen <joni.tahvanainen@csc.fi>
2022-09-02 09:24:13 +0300

    Add alerts to copyToClipboard
        * All copyToClipboard() now have alerts

---
+++ webui/src/components/filetoken.jsx
@@ -0,0 +1,44 @@
+import React from 'react/lib/ReactWithAddons';
+import { serverCache, apiUrls } from '../data/server.js';
+import { copyToClipboard } from './editfiles.jsx';
+
+const FileToken = React.createClass({
+    getInitialState() {
+        return {
+            fetching: false,
+            token: null,
+            zipFile : false
+        }
+    },
+   
+    getUrl(bucket,name,token) {
+        return apiUrls.tempFileAccessLink(bucket,name,token)
+    },
+    
+
+    render() {
+        if (!this.props) {
+            return null
+        }
+        let url=""
+        let file_name="files.zip"
+        if (!this.props.zipFile){
+            url=this.getUrl(this.props.file.bucket, this.props.file.key, this.props.token)     
+            file_name=this.props.file.key     
+        } 
+        else {
+            url = this.getUrl(this.props.buckets[0], "files.zip", this.props.token) + "&all=1"
+        } 
+        
+        return(
+            <button type="button" className="btn btn-default btn-xs remove" onClick={() => {copyToClipboard(window.location.origin + url , "'"+file_name +"' temporary link");}}
+                title="Copy Temporary Access Link">
+                <i className="glyphicon glyphicon-link" />
+            </button>
+        )
+        
+
+    }
+})
+
+export default FileToken;

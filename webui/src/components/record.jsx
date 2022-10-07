d36872f954f54abbf347059615145528eb3abab7
Giacomo Furlan <giacomo.furlan@csc.fi>
2022-10-07 14:57:11 +0300

    disable some integration

--- webui/src/components/record.jsx
+++ webui/src/components/record.jsx
@@ -444,7 +444,7 @@ const Record = React.createClass({
             }
             return (
                 <div className="well">
-                <div className="row">
+                {/* <div className="row">
                     <h3 className="col-sm-9">
                         { 'Share' }
                     </h3>
@@ -453,7 +453,7 @@ const Record = React.createClass({
                     <div className="col-sm-9">
                         {this.renderShareButtons(doi)}
                     </div>
-                </div>
+                </div> */}
                 <div className="row">
                     <h3 className="col-sm-9">
                         { 'Cite as' }
